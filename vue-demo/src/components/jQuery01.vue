947cf75173218ad710c4da0410209a9500846651
Hushyo <Hushsama@hotmail.com>
2024-10-17 23:26:07 +0800

    update

---
+++ vue-demo/src/components/jQuery01.vue
@@ -0,0 +1,6 @@
+<template>
+{{ msg }}
+</template>
+
+
+
--- vue-demo/src/main.js
+++ vue-demo/src/main.js
@@ -1,6 +1,6 @@
 
 
 import { createApp } from 'vue'
-import App from './components/v-model.vue'
+import App from './components/jQuery01.vue'
 
 createApp(App).mount('#app')
